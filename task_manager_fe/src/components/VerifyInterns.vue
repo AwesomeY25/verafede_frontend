aa97fe5c93298b5fecae838e8b870480d7eac097
sophia-algenio <160018185+sophia-algenio@users.noreply.github.com>
2024-05-01 16:59:27 +0800

    added modal template

--- task_manager_fe/src/components/VerifyInterns.vue
+++ task_manager_fe/src/components/VerifyInterns.vue
@@ -105,6 +105,26 @@
         </nav>
       </div>
     </div>
+
+    <!-- Modal -->
+    <template>
+      <div class="modal" tabindex="-1" role="dialog" id="ndaModal">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">NDA File Content</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <pre>{{ modalContent }}</pre>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
   </template>
   
   <script>
